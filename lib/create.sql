25b310d88283b1b7fff450b42b24cac4bf7452f7
Milos Popovic <milos_popovic@outlook.com>
2020-09-07 03:33:47 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,14 +1,14 @@
 CREATE TABLE projects (
-    id INTEGER PRIMARY KEY, 
-    title TEXT, 
+    id INTEGER PRIMARY KEY,
+    title TEXT,
     category TEXT,
-    funding_goal INTEGER, 
-    start_date DATE, 
+    funding_goal INTEGER,
+    start_date DATE,
     end_date DATE
 );
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY, 
-    name TEXT, 
+    id INTEGER PRIMARY KEY,
+    name TEXT,
     age INTEGER
 );
 CREATE TABLE pledges (
